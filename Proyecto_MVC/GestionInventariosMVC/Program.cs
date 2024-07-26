2c9ffa7a0445e96fda69c1894e6fd191e1c49d3b
birdkron <js8215001@gmail.com>
2024-07-26 14:29:25 -0600

    Configuracion conexion bdd

--- Proyecto_MVC/GestionInventariosMVC/Program.cs
+++ Proyecto_MVC/GestionInventariosMVC/Program.cs
@@ -3,6 +3,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+//builder.Services.AddDbContext<>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("/*DefaultCN*/")));
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
