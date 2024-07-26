ffd71b3bd9fe2be224244cc0385a9bf242ad9a78
birdkron <js8215001@gmail.com>
2024-07-26 15:04:56 -0600

    Update CategoriasSeed.cs

--- Proyecto_MVC/GestionInventariosMVC/Models/Seeds/CategoriasSeed.cs
+++ Proyecto_MVC/GestionInventariosMVC/Models/Seeds/CategoriasSeed.cs
@@ -8,9 +8,9 @@ namespace GestionInventariosMVC.Models.Seeds
         public void Configure(EntityTypeBuilder<Categoria> builder)
         {
             builder.HasData(
-                new Categoria { Codigo = "CAT_ELECTRONICA", Nombre = "Electrónica" },
-                new Categoria { Codigo = "CAT_ROPA", Nombre = "Ropa" },
-                new Categoria { Codigo = "CAT_ALIMENTOS", Nombre = "Alimentos" });
+                new Categoria { Codigo = "CAT_ELECTRONICA", Nombre = "Electrónica",Descripcion = "Electrónica" },
+                new Categoria { Codigo = "CAT_ROPA", Nombre = "Ropa", Descripcion = "Ropa" },
+                new Categoria { Codigo = "CAT_ALIMENTOS", Nombre = "Alimentos", Descripcion = "Alimentos" });
         }
     }
 }
