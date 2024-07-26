a0eea9b1bb7b95a61510078361b5daacda750e6a
birdkron <js8215001@gmail.com>
2024-07-26 15:22:33 -0600

    Agregados vistas y controlador para CRUD productos

---
+++ Proyecto_MVC/GestionInventariosMVC/Controllers/ProductosController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using GestionInventariosMVC.Models;
+
+namespace GestionInventariosMVC.Controllers
+{
+    public class ProductosController : Controller
+    {
+        private readonly InventarioDBContext _context;
+
+        public ProductosController(InventarioDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Productos
+        public async Task<IActionResult> Index()
+        {
+            var inventarioDBContext = _context.Productos.Include(p => p.Categoria);
+            return View(await inventarioDBContext.ToListAsync());
+        }
+
+        // GET: Productos/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var producto = await _context.Productos
+                .Include(p => p.Categoria)
+                .FirstOrDefaultAsync(m => m.Codigo == id);
+            if (producto == null)
+            {
+                return NotFound();
+            }
+
+            return View(producto);
+        }
+
+        // GET: Productos/Create
+        public IActionResult Create()
+        {
+            ViewData["CodigoCategoria"] = new SelectList(_context.Categorias, "Codigo", "Codigo");
+            return View();
+        }
+
+        // POST: Productos/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Codigo,Nombre,Descripcion,Precio,Stock,CodigoCategoria")] Producto producto)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(producto);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CodigoCategoria"] = new SelectList(_context.Categorias, "Codigo", "Codigo", producto.CodigoCategoria);
+            return View(producto);
+        }
+
+        // GET: Productos/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var producto = await _context.Productos.FindAsync(id);
+            if (producto == null)
+            {
+                return NotFound();
+            }
+            ViewData["CodigoCategoria"] = new SelectList(_context.Categorias, "Codigo", "Codigo", producto.CodigoCategoria);
+            return View(producto);
+        }
+
+        // POST: Productos/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("Codigo,Nombre,Descripcion,Precio,Stock,CodigoCategoria")] Producto producto)
+        {
+            if (id != producto.Codigo)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(producto);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProductoExists(producto.Codigo))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CodigoCategoria"] = new SelectList(_context.Categorias, "Codigo", "Codigo", producto.CodigoCategoria);
+            return View(producto);
+        }
+
+        // GET: Productos/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var producto = await _context.Productos
+                .Include(p => p.Categoria)
+                .FirstOrDefaultAsync(m => m.Codigo == id);
+            if (producto == null)
+            {
+                return NotFound();
+            }
+
+            return View(producto);
+        }
+
+        // POST: Productos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var producto = await _context.Productos.FindAsync(id);
+            if (producto != null)
+            {
+                _context.Productos.Remove(producto);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProductoExists(string id)
+        {
+            return _context.Productos.Any(e => e.Codigo == id);
+        }
+    }
+}
